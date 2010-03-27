87d878f2fc608996cf745ca113980d2b0ec75f0d
Jack Danger Canty <gitcommit@6brand.com>
2010-03-27 16:14:11 -0700

    adding link to the client

--- shawty.rb
+++ shawty.rb
@@ -13,14 +13,14 @@ require 'alphadecimal'
 get '/' do
   %Q{
     <body style='line-height: 1.8em; font-family: Archer, Museo, Helvetica, Georgia; font-size 25px; text-align: center; padding-top: 20%;'>
-      Post to '/' to save a url and receive a plaintext short url in response
-      <br />
-      Example: <pre style='font-family: Iconsolata, monospace;background-color:#EEE'>curl -X POST http://#{request.host}/http://some.url/at.someplace</pre>
+      Post to '/' to save a url and receive a plaintext short url in response. Example:
+      <pre style='font-family: Iconsolata, monospace;background-color:#EEE'>curl -X POST http://#{request.host}/http://some.url/at.someplace</pre>
       <br />
       <form action=/ method=POST onsubmit='if(\"\"==this.url.value)return false;else{this.action=\"/\"+this.url.value}'>
         <input type=text name='url' />
         <input type=submit value='Get Shawty' />
       </form>
+      <small>Also, try <a href='http://github.com/JackDanger/shawty-client'>the official Ruby client</a></small>
     </body
 }
 end
